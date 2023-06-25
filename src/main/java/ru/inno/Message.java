b75d034b10168d1c544275b048872bb44ea819df
loba <loba@protei.ru>
2023-06-25 18:19:27 +0300

    Classes' names were changed

--- src/main/java/ru/inno/Message.java
+++ src/main/java/ru/inno/Message.java
@@ -1,23 +1,21 @@
 package ru.inno;
 
-public class MessageData {
+public class Message {
 
     public final int threeLettersLength = 62;
     public final int spaceLength = 12;
     public final int lettersAmount = 15;
     public final int spacesAmount = 3;
-    public int fenceLength;
 
-    public MessageData() {
+    public Message() {
     }
 
-    public boolean isFenceLengthSuitable (int fenceLength) {
+    public boolean isFenceLengthSuitable(int fenceLength) {
         int index = fenceLength / (threeLettersLength / 3 * lettersAmount + spaceLength * spacesAmount);
         if (index < 1) {
             System.out.println("Посление не поместится на забор :(");
             return false;
-        }
-        else
+        } else
             System.out.println("Посление поместится на забор :)");
         return true;
     }
